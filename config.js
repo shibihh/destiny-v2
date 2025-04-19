1aef431ae44a8dc3a5455fb4867da18862b57b48
shibihh <shibilka05@gmail.com>
2025-04-19 19:15:05 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+const { ActivityType } = require('discord.js');
+
+module.exports = {
+  ownerId: '1004206704994566164',
+  status: {
+    rotateDefault: [
+  
+      { name: '/cmnd', type: ActivityType.Listening },
+    ],
+    songStatus: true
+  },
+  spotifyClientId: "f71a3da30e254962965ca2a89d6f74b9",
+  spotifyClientSecret: "199a619d22dd4e55a4a2c1a7a3d70e63",
+}
